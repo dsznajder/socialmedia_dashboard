a247ae7d13242fc9e32a97f773ec389e108cc109
Tomasz Romik <manetamajster@gmail.com>
2017-04-20 16:29:34 +0200

    Prevent user from adding more than one like (refs #2)

--- client/app/Dashboard/reducers/LikesReducer.js
+++ client/app/Dashboard/reducers/LikesReducer.js
@@ -3,7 +3,11 @@ import {ADD_LIKE, GET_LIKES} from '../actions/types.js'
 export default (state = [], action) => {
   switch (action.type) {
     case ADD_LIKE:
-      return [action.payload, ...state]
+      // Find like index and cast it to boolean
+      // 0 is considered false so we flip bits so it becomes -1 (equals to true)
+      // -1 (not found) gets converted to 0 (equals to false)
+      const exists = !!~Array.findIndex(state, like => action.payload.id === like.id);
+      return exists ? state : [action.payload, ...state]
     case GET_LIKES:
       return action.payload
     default:
