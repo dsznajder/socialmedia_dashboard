ded47771f62091b6f3310bcc14fd72b62cc0b89f
Tomasz Romik <manetamajster@gmail.com>
2017-04-25 10:32:58 +0200

    Split upload previews into separate component

---
+++ client/app/Dashboard/components/PreviewsList.jsx
@@ -0,0 +1,27 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class PreviewsList extends Component {
+  static propTypes = {
+    files: PropTypes.array.isRequired,
+    removeFile: PropTypes.func.isRquired
+  }
+
+  render() {
+    return (
+      <div className='upload-preview'>
+        {this.props.files.map(file => {
+          return (
+            <img
+              alt={file.name}
+              height='60'
+              key={file.preview}
+              onDoubleClick={this.removeFile}
+              src={file.preview}
+              title='Double click to remove'
+            />
+          )
+        })}
+      </div>
+    );
+  }
+}
